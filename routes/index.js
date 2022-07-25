4234a2c4598023dfefc0b3bd602ee6b40f29ef26
Emilio Coroy <becgonzalez25@gmail.com>
2022-07-24 20:48:30 -0600

    Creación de layout

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,10 @@
 import express from 'express';
+import homeRouter from './home.router.js';
 
 const routerAPP = (app) => {
     const router = express.Router();
     app.use('/', router);
-    router.use('/', (req, res) => {
-        res.send("Bienvenido");
-    });
+    router.use('/', homeRouter);
 };
 
 export default routerAPP;
